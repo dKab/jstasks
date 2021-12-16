c066a2a598e28e61fc654f789ced3b2cc72a0c80
Dima <dkabardinov@gmail.com>
2021-12-16 13:44:43 +0300

    адаптировать роуты под GH pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
 import Task from '../components/Task'
-import Counter from '../components/Counter'
 import NoMatch from '../components/NoMatch'
 import NavBar from '../components/NavBar/NavBar'
 import { Header } from '../components/Header/Header'
@@ -12,10 +11,9 @@ const routes = (
     <NavBar />
     <Switch>
       <Route exact path="/">
-        <Redirect to="/tasks/1" />
+        <Redirect to="/jstasks/tasks/1" />
       </Route>
-      <Route path="/tasks/:id" component={Task} />
-      <Route path="/counter" component={Counter} />
+      <Route path="/jstasks/tasks/:id" component={Task} />
       <Route component={NoMatch} />
     </Switch>
   </div>
